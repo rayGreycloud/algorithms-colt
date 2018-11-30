260c48266afbac66024cd1e4ee549e95906bc582
rayGreycloud <rayGreycloud@gmail.com>
2018-11-29 16:30:58 -0800

    Exercise 2 pt 1

---
+++ ex2-sumZero-naive.js
@@ -0,0 +1,12 @@
+function sumZero(arr) {
+  for (let i = 0; i < arr.length; i++) {
+    for (let j = i + 1; j < arr.length; j++) {
+      if (arr[i] + arr[j] === 0) {
+        return [arr[i], arr[j]];
+      }
+    }
+  }
+}
+
+console.log(sumZero([-3, -2, -1, 0, 1, 2, 3]));
+console.log(sumZero([-2, 0, 1, 3]));
