3029dc07d5da687a5614f5c2b81b8934e9228a41
rayGreycloud <rayGreycloud@gmail.com>
2018-12-04 12:46:30 -0800

    Add more test cases

--- 36-findLongestSubstring.js
+++ 36-findLongestSubstring.js
@@ -26,4 +26,7 @@ function findLongestSubstring(str) {
 console.log(findLongestSubstring('')); // 0
 console.log(findLongestSubstring('rithmschool')); // 7
 console.log(findLongestSubstring('thisisawesome')); // 6
+console.log(findLongestSubstring('thecatinthehat')); // 7
 console.log(findLongestSubstring('bbbbbb')); // 1
+console.log(findLongestSubstring('longestsubstring')); // 8
+console.log(findLongestSubstring('thisishowwedoit')); // 6
