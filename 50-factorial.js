d2971ac7f5fa128f0627841541b1c82c8f09791a
rayGreycloud <rayGreycloud@gmail.com>
2018-12-04 13:45:29 -0800

    factorial function

---
+++ 50-factorial.js
@@ -0,0 +1,11 @@
+// factorial function
+
+function factorial(num) {
+  if (num === 0) return 1;
+  return num * factorial(num - 1);
+}
+
+console.log(factorial(1)); // 1
+console.log(factorial(2)); // 2
+console.log(factorial(4)); // 4
+console.log(factorial(7)); // 5040
