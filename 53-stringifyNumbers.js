2abc55db643ba312c60165d781bcc475ab4c4063
rayGreycloud <rayGreycloud@gmail.com>
2018-12-09 09:12:17 -0800

    Recursive func to stringify numbers in nested object

--- 53-stringifyNumbers.js
+++ 53-stringifyNumbers.js
@@ -2,7 +2,7 @@ function stringifyNumbers(obj) {
   let newObj = {};
 
   for (let key in obj) {
-    if (typeof obj[key] === 'object') {
+    if (typeof obj[key] === 'object' && !Array.isArray(obj[key])) {
       newObj[key] = stringifyNumbers(obj[key]);
     } else if (typeof obj[key] === 'number') {
       newObj[key] = obj[key].toString();
