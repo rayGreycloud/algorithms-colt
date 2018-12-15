759b43d8e6b74dd84a52ffee717e210f60d5af17
rayGreycloud <rayGreycloud@gmail.com>
2018-12-15 14:08:37 -0800

    Correct pivot helper

--- 94-pivotHelper.js
+++ 94-pivotHelper.js
@@ -1,14 +1,13 @@
-function pivot(array, start = 0, end = array.length + 1) {
-  function swap(arr, a, b) {
-    let temp = arr[a];
-    arr[a] = arr[b];
-    arr[b] = temp;
-  }
+// swap helper
+function pivot(array, start = 0, end = array.length - 1) {
+  const swap = (arr, idx1, idx2) => {
+    [arr[idx1], arr[idx2]] = [arr[idx2], arr[idx1]];
+  };
 
   let pivot = array[start];
   let swapIdx = start;
 
-  for (let i = start + 1; i < array.length - 1; i++) {
+  for (let i = start + 1; i <= end; i++) {
     if (pivot > array[i]) {
       swapIdx++;
       swap(array, swapIdx, i);
@@ -22,4 +21,4 @@ function pivot(array, start = 0, end = array.length + 1) {
 
 let arr = [5, 2, 1, 8, 4, 7, 6, 3];
 
-console.log(pivot(arr)); // 3
+console.log(pivot(arr)); // 4
