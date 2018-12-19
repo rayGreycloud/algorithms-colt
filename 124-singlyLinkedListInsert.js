ce033a1e61afea42dc501c5806357f2135297a9b
rayGreycloud <rayGreycloud@gmail.com>
2018-12-19 11:56:16 -0800

    Insert method for singly linked list

---
+++ 124-singlyLinkedListInsert.js
@@ -0,0 +1,127 @@
+class Node {
+  constructor(val) {
+    this.val = val;
+    this.next = null;
+  }
+}
+
+class SinglyLinkedList {
+  constructor() {
+    this.head = null;
+    this.tail = null;
+    this.length = 0;
+  }
+
+  push(val) {
+    let newNode = new Node(val);
+    if (!this.head) {
+      this.head = newNode;
+      this.tail = this.head;
+    } else {
+      this.tail.next = newNode;
+      this.tail = newNode;
+    }
+    this.length++;
+
+    return this;
+  }
+
+  pop() {
+    if (!this.head) return undefined;
+
+    let current = this.head;
+    let newTail = current;
+
+    while (current.next) {
+      newTail = current;
+      current = current.next;
+    }
+
+    this.tail = newTail;
+    this.tail.next = null;
+    this.length--;
+
+    if (this.length === 0) {
+      this.head = null;
+      this.tail = null;
+    }
+
+    return current;
+  }
+
+  shift() {
+    if (!this.head) return undefined;
+    let oldHead = this.head;
+    this.head = oldHead.next;
+    this.length--;
+    if (this.length === 0) {
+      this.tail = null;
+    }
+
+    return oldHead;
+  }
+
+  unshift(val) {
+    let newNode = new Node(val);
+    if (!this.head) {
+      this.head = newNode;
+      this.tail = this.head;
+    } else {
+      newNode.next = this.head;
+      this.head = newNode;
+    }
+    this.length++;
+    return this;
+  }
+
+  get(idx) {
+    if (idx < 0 || idx >= this.length) return null;
+
+    var counter = 0;
+    var current = this.head;
+
+    while (counter < idx) {
+      counter++;
+      current = current.next;
+    }
+
+    return current;
+  }
+
+  set(idx, val) {
+    let foundNode = this.get(idx);
+    if (foundNode) {
+      foundNode.val = val;
+      return true;
+    }
+
+    return false;
+  }
+
+  insert(idx, val) {
+    if (idx < 0 || idx > this.length) return false;
+    if (idx === this.length) return !!this.push(val);
+    if (idx === 0) return !!this.unshift(val);
+
+    let newNode = new Node(val);
+    let prev = this.get(idx - 1);
+    let oldNext = prev.next;
+    prev.next = newNode;
+    newNode.next = oldNext;
+    this.length++;
+
+    return true;
+  }
+}
+
+let list = new SinglyLinkedList();
+list.unshift('Candy');
+list.unshift('Balls');
+list.unshift('Apples');
+console.log(list.insert(2, 'Beagles'));
+console.log(list);
+console.log(list.get(2));
+console.log(list.insert(0, 'Zebras'));
+console.log(list);
+console.log(list.insert(5, 'Dogs'));
+console.log(list);
