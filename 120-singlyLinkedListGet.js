decba5a20a2859358b29f0476ecd33190d889f6d
rayGreycloud <rayGreycloud@gmail.com>
2018-12-19 11:32:20 -0800

    Get method for singly linked list

--- 120-singlyLinkedListGet.js
+++ 120-singlyLinkedListGet.js
@@ -87,6 +87,16 @@ class SinglyLinkedList {
 
     return current;
   }
+
+  set(idx, val) {
+    let foundNode = this.get(idx);
+    if (foundNode) {
+      foundNode.val = val;
+      return true;
+    }
+
+    return false;
+  }
 }
 
 let list = new SinglyLinkedList();
