2f423dda7f9f13b46a49b3657384bee0e96d109f
rayGreycloud <rayGreycloud@gmail.com>
2018-12-22 15:34:38 -0800

    get method for doubly linked list

---
+++ 142-doublyLinkedListGet.js
@@ -0,0 +1,113 @@
+class Node {
+  constructor(val) {
+    this.val = val;
+    this.next = null;
+    this.prev = null;
+  }
+}
+
+class DoublyLinkedList {
+  constructor() {
+    this.head = null;
+    this.tail = null;
+    this.length = 0;
+  }
+
+  push(val) {
+    let newNode = new Node(val);
+    if (this.length === 0) {
+      this.head = newNode;
+      this.tail = newNode;
+    } else {
+      this.tail.next = newNode;
+      newNode.prev = this.tail;
+      this.tail = newNode;
+    }
+    this.length++;
+
+    return this;
+  }
+
+  pop() {
+    if (!this.head) return undefined;
+    let currentTail = this.tail;
+
+    if (this.length === 1) {
+      this.head = null;
+      this.tail = null;
+    } else {
+      this.tail = currentTail.prev;
+      this.tail.next = null;
+      currentTail.prev = null;
+    }
+    this.length--;
+
+    return currentTail;
+  }
+
+  shift() {
+    if (this.length === 0) return undefined;
+
+    let oldHead = this.head;
+
+    if (this.length === 1) {
+      this.head = null;
+      this.tail = null;
+    } else {
+      this.head = oldHead.next;
+      this.head.prev = null;
+      oldHead.next = null;
+    }
+    this.length--;
+
+    return oldHead;
+  }
+
+  unshift(val) {
+    let newNode = new Node(val);
+
+    if (!this.head) {
+      this.head = newNode;
+      this.tail = this.head;
+    } else {
+      this.head.prev = newNode;
+      newNode.next = this.head;
+      this.head = newNode;
+    }
+    this.length++;
+
+    return this;
+  }
+
+  get(idx) {
+    if (idx < 0 || idx >= this.length) return null;
+
+    var count, current;
+
+    if (idx <= this.length / 2) {
+      current = this.head;
+      count = 0;
+      while (count !== idx) {
+        current = current.next;
+        count++;
+      }
+    } else {
+      current = this.tail;
+      count = this.length - 1;
+      while (count !== idx) {
+        current = current.prev;
+        count--;
+      }
+    }
+
+    return current;
+  }
+}
+
+let list = new DoublyLinkedList();
+list.unshift('Harry');
+list.unshift('Ron');
+list.unshift('Hermione');
+console.log(list);
+console.log(list.get(1));
+console.log(list.get(0));
