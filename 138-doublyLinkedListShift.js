672c7254548ae4531470a9e57ad64e72f93a9db0
rayGreycloud <rayGreycloud@gmail.com>
2018-12-22 14:09:55 -0800

    shift method for doubly linked list

---
+++ 138-doublyLinkedListShift.js
@@ -0,0 +1,73 @@
+class Node {
+  constructor(val) {
+    this.val = val;
+    this.next = null;
+    this.prev = null;
+  }
+}
+
+class DoublyLinkedList {
+  constructor() {
+    this.head = null;
+    this.tail = null;
+    this.length = 0;
+  }
+
+  push(val) {
+    let newNode = new Node(val);
+    if (this.length === 0) {
+      this.head = newNode;
+      this.tail = newNode;
+    } else {
+      this.tail.next = newNode;
+      newNode.prev = this.tail;
+      this.tail = newNode;
+    }
+    this.length++;
+
+    return this;
+  }
+
+  pop() {
+    if (!this.head) return undefined;
+    let currentTail = this.tail;
+
+    if (this.length === 1) {
+      this.head = null;
+      this.tail = null;
+    } else {
+      this.tail = currentTail.prev;
+      this.tail.next = null;
+      currentTail.prev = null;
+    }
+    this.length--;
+
+    return currentTail;
+  }
+
+  shift() {
+    if (this.length === 0) return undefined;
+
+    let oldHead = this.head;
+
+    if (this.length === 1) {
+      this.head = null;
+      this.tail = null;
+    } else {
+      this.head = oldHead.next;
+      this.head.prev = null;
+      oldHead.next = null;
+    }
+    this.length--;
+
+    return oldHead;
+  }
+}
+
+let list = new DoublyLinkedList();
+list.push('Harry');
+list.push('Ron');
+list.push('Hermione');
+console.log(list);
+list.shift();
+console.log(list);
